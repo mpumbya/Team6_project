82d6a196cc46de28f471a340867ae1581d2dd64e
rodney <mpumbya.rodney@gmail.com>
2017-08-18 14:05:19 +0300

    local changes

--- cons.py
+++ cons.py
@@ -1,6 +1,6 @@
 import requests
-url = 'http://34.207.10.230:3000/posts/8'
-data = '{ "id": }'
+url = 'http://34.207.10.230:3000/posts'
+data = '{ "id" }'
 response = requests.get(url, data=data,headers={"Content-Type": "application/json"})
 print(response)
 #sid=response.json()['platform']['login']['sessionId']   
