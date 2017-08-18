11d8565bde69e73788730bb464c3988c939260b2
Jerry-Edebua <jeredebua@gmail.com>
2017-08-18 13:41:43 +0300

    Implementing add_post

--- cons.py
+++ cons.py
@@ -1,8 +1,29 @@
 import requests
-url = 'http://34.207.10.230:3000/posts'
-data = '{ "id" }'
-response = requests.get(url, data=data,headers={"Content-Type": "application/json"})
-print(response)
-#sid=response.json()['platform']['login']['sessionId']   
-print(response.text)
-#print(sid)
+
+def view_posts():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url, headers={"Content-Type":"application/json"})
+    if response.status_code == 200:
+        print ('Title |  Body')
+        for post in response.json():
+            print ('{} | {}'.format(post['title'], post['body']))
+    else:
+        print("Check internet connection")
+
+def add_post(title, body):
+    url='http://34.207.10.230:3000/posts'
+    response = requests.post(url, data = { 'title':title, 'body':body,'userId': 1 })
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Could not post, check internet connection")
+    pass
+
+def view_comment_on_post():
+    pass
+
+def add_comment_on_post():
+    pass
+
+if __name__ == '__main__':
+    add_post('Team 6', 'Top posts')
