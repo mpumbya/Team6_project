99fce33051e976e68b50af8e80fceecd579ba2b0
flacode <flavianshemerirwe@gmail.com>
2017-08-18 13:17:01 +0300

    FIXTURE: Add required methods

--- cons.py
+++ cons.py
@@ -1,8 +1,28 @@
 import requests
-url = 'http://34.207.10.230:3000/posts'
-data = '{ "id" }'
-response = requests.get(url, data=data,headers={"Content-Type": "application/json"})
-print(response)
-#sid=response.json()['platform']['login']['sessionId']   
-print(response.text)
-#print(sid)
+
+
+def view_posts():
+    url='https://jsonplaceholder.typicode.com/posts'
+    response=requests.get(url, headers={"Content-Type":"application/json"})
+    if response.status_code == 200:
+        print ('Title |  Body')
+        for post in response.json():
+            print ('{} | {}'.format(post['title'], post['body']))
+    else:
+        print ("Check internet connection")
+
+
+def add_posts():
+    pass
+
+
+def view_comment_on_post():
+    pass
+
+
+def add_comment_on_post():
+    pass
+
+
+if __name__ == '__main__':
+    view_posts()
