d2a70698d5e694f007178746d99245591e4cee22
flacode <flavianshemerirwe@gmail.com>
2017-08-18 13:45:04 +0300

    FEATURE: Add method to view posts

--- cons.py
+++ cons.py
@@ -2,6 +2,7 @@ import requests
 
 
 def view_posts():
+    """Method to view all posts on the feed"""
     url='https://jsonplaceholder.typicode.com/posts'
     response=requests.get(url, headers={"Content-Type":"application/json"})
     if response.status_code == 200:
@@ -16,8 +17,17 @@ def add_posts():
     pass
 
 
-def view_comment_on_post():
-    pass
+def view_comment_on_post(post_id):
+    """Method to view comments on post with post_id"""
+    url='https://jsonplaceholder.typicode.com/posts/'+str(post_id)+'/comments'
+    response=requests.get(url, headers={"Content-Type":"application/json"})
+    if response.status_code == 200:
+        print "Comments on Post"
+        print ('Comment by |  Body')
+        for comment in response.json():
+            print ('{} | {}'.format(comment['name'], comment['body']))
+    else:
+        print ("Check internet connection")
 
 
 def add_comment_on_post():
@@ -26,3 +36,4 @@ def add_comment_on_post():
 
 if __name__ == '__main__':
     view_posts()
+    view_comment_on_post(1)
